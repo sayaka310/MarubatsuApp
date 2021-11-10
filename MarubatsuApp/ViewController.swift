7367c5aa1653f01b4abf64821dcd9074f208e9c0
sayaka310 <syk3887@yahoo.co.jp>
2021-11-10 11:52:15 +0900

    課題完了

--- MarubatsuApp/ViewController.swift
+++ MarubatsuApp/ViewController.swift
@@ -8,32 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var questionLabel: UILabel!
     
     var currentQuestionNum: Int = 0
     
-    let questions: [[String: Any]] = [
-            [
-                "question": "iPhoneアプリを開発する統合環境はZcodeである",
-                "answer": false
-            ],
-            [
-                "question": "Xcode画面の右側にはユーティリティーズがある",
-                "answer": true
-            ],
-            [
-                "question": "UILabelは文字列を表示する際に利用する",
-                "answer": true
-            ]
-        ]
+    var questions: [[String: Any]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-                showQuestion()
+        let userDefaults = UserDefaults.standard
+        if userDefaults.object(forKey: "add") != nil {
+            questions = userDefaults.object(forKey: "add") as! [[String:Any]]
+        }
+        
+        showQuestion()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let userDefaults = UserDefaults.standard
+        if userDefaults.object(forKey: "add") != nil {
+            questions = userDefaults.object(forKey: "add") as! [[String:Any]]
+        }
+        
+        showQuestion()
+    }
+    
     @IBAction func tappedNoButton(_ sender: Any) {
         checkAnswer(yourAnswer: false)
     }
@@ -43,52 +45,55 @@ class ViewController: UIViewController {
     }
     
     func showQuestion() {
+        
+        if questions.count == 0 {
+            questionLabel.text = "問題がありません。問題を作りましょう。"
+        } else {
             let question = questions[currentQuestionNum]
-
+            
             if let que = question["question"] as? String {
                 questionLabel.text = que
             }
         }
+        
+    }
     
     func checkAnswer(yourAnswer: Bool) {
-
-           let question = questions[currentQuestionNum]
-
-           if let ans = question["answer"] as? Bool {
-
-               if yourAnswer == ans {
-                   // 正解
-                   // currentQuestionNumを1足して次の問題に進む
-                   currentQuestionNum += 1
-                   showAlert(message: "正解！")
-               } else {
-                   // 不正解
-                   showAlert(message: "不正解…")
-
-               }
-           } else {
-               print("答えが入ってません")
-               return
-           }
-
-           // 問題を表示します。
-        if currentQuestionNum >= questions.count {
-                    currentQuestionNum = 0
-                }
         
+        let question = questions[currentQuestionNum]
+        
+        if let ans = question["answer"] as? Bool {
+            
+            if yourAnswer == ans {
+                // 正解
+                // currentQuestionNumを1足して次の問題に進む
+                currentQuestionNum += 1
+                showAlert(message: "正解！")
+            } else {
+                // 不正解
+                showAlert(message: "不正解…")
+                
+            }
+        } else {
+            print("答えが入ってません")
+            return
+        }
+        
+        // 問題を表示します。
+        if currentQuestionNum >= questions.count {
+            currentQuestionNum = 0
+        }
         
         //正解であれば次の問題が、不正解であれば同じ問題が再表示されます。
-           showQuestion()
-       }
+        showQuestion()
+    }
     
     func showAlert(message: String) {
-            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-            let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
-            alert.addAction(close)
-            present(alert, animated: true, completion: nil)
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
+        alert.addAction(close)
+        present(alert, animated: true, completion: nil)
         
-        
-        }
-
+    }
     
 }
